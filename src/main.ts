bfc1b79f3d91d049e645f9dfff47d753c4d538b7
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2024-02-07 01:10:56 -0300

    refactor: clean main.ts entrypoint, instantiate and starts aplication server

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,11 @@
-import express, { Request, Response } from 'express';
-import CreateTransacion from './application/CreateTransaction';
-import GetTransaction from './application/GetTransaction';
+import ExpressAdapter from './infra/api/ExpressAdapter';
+import Router from './infra/api/Router';
+import PostgresSQLAdapter from './infra/database/PostgreSQLAdapter';
 import TransactionDatabaseRepository from './infra/repository/TransactionDatabaseRepository';
-const app = express();
-app.use(express.json());
 
-const transactionRepository = new TransactionDatabaseRepository();
-
-app.post('/transactions', async (req: Request, res: Response) => {
-  const createTransaction = new CreateTransacion(transactionRepository);
-  await createTransaction.execute(req.body);
-  res.end();
-})
-
-app.get('/transactions/:code', async (req: Request, res: Response) => { 
-  const getTransaction = new GetTransaction();
-  const transaction = await getTransaction.execute(req.params.code);
-  res.json(transaction);
-});
-
-app.listen(3000);
+const connection = new PostgresSQLAdapter();
+const transactionRepository = new TransactionDatabaseRepository(connection);
+const httpServer = new ExpressAdapter();
+const router = new Router(httpServer, transactionRepository);
+router.init();
+httpServer.listen(3000)
