3f756e40dbb8a96b2aaedf57fc8276fcc6b523af
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2024-02-07 01:11:54 -0300

    refactor: add in memory repository to mock database

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import ExpressAdapter from './infra/api/ExpressAdapter';
 import Router from './infra/api/Router';
 import PostgresSQLAdapter from './infra/database/PostgreSQLAdapter';
 import TransactionDatabaseRepository from './infra/repository/TransactionDatabaseRepository';
+import TransactionMemoryRepository from './infra/repository/TransactionMemoryRepository';
 
 const connection = new PostgresSQLAdapter();
-const transactionRepository = new TransactionDatabaseRepository(connection);
+// const transactionRepository = new TransactionDatabaseRepository(connection);
+const transactionRepository = new TransactionMemoryRepository();
 const httpServer = new ExpressAdapter();
 const router = new Router(httpServer, transactionRepository);
 router.init();
